582d71f990226c1f0c41ac354d142cf6729f5062
reinankaua <69223145+reinankaua@users.noreply.github.com>
2023-05-09 23:59:12 +0000

    declarando uma lista e percorrendo a mesma com um for

--- aula-reforco/main.c
+++ aula-reforco/main.c
@@ -4,9 +4,9 @@
 int main(void) {
   int idade = 20;
   int idade_fut = idade + 10;
-  bool teste;
   printf("%d\n", idade_fut);
 
+  bool teste;
   if(idade >= 34 && idade_fut <= 50){
     teste = true;
   }else{
@@ -16,9 +16,15 @@ int main(void) {
   if(teste){
     printf("Tá ficando veio em nego");
   }else{
-    printf("Cheirando a leite");
+    printf("Cheirando a leite\n");
   }
 
-  
+  int lista[5] = {5, 6, 8, 4, 9};
+  int soma = 0;
+  for(int index = 0; index < 5; index++){
+    printf("Valor da posição %d da lista é %d\n", index, lista[index]);
+    soma += lista[index];
+  }
+  printf("\nA soma dos valores da lista é %d\n", soma);
   return 0;
 }
