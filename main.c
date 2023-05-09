ebbe204859f710e08cc7701fa8063dfd56614d31
reinankaua <21065235-reinankaua@users.noreply.replit.com>
2023-05-09 23:33:33 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void) {
+  int idade = 20;
+  int idade_fut = idade + 10;
+  bool teste;
+  printf("%d\n", idade_fut);
+
+  if(idade >= 34 && idade_fut <= 50){
+    teste = true;
+  }else{
+    teste = false;
+  }
+
+  if(teste){
+    printf("Tá ficando veio em nego");
+  }else{
+    printf("Cheirando a leite");
+  }
+  
+  return 0;
+}
